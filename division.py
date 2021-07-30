8ab1f32305cb690c29af9474fd56f1fc176a0e50
duyvt <vuthanhduy93@gmail.com>
2021-07-30 17:20:04 +0700

    nested List fixed

--- division.py
+++ division.py
@@ -4,8 +4,8 @@
 # The first line should contain the result of integer division,  a//b . 
 # The second line should contain the result of float division, a / b.
 
-a = int(input('nhap vvao so a = '))
-b = int(input('nhap vvao so b = '))
+a = int(input('nhap vao so a = '))
+b = int(input('nhap vao so b = '))
 print(f' integer division = {a//b}')
 print(f'float division result = {a/b}')
 #add a comment
--- nestedList.py
+++ nestedList.py
@@ -1,7 +1,19 @@
 n = int (input('insert student Number: '))
-ans = []
+
+students = []
 for a in range(n):
     name = input('student name: ')
     score = int(input('student score: '))
-    ans.append([name, score])
-print(ans)
+    students.append([name, score])
+print(students)
+students = sorted(students, key=lambda x: x[1])
+# print(students[n-2][0])
+secondLowest = sorted(list(set(x[1] for x in students)))[1]
+# print(secondLowest)
+desireStudent = []
+for x in students:
+    if x[1]== secondLowest:
+        desireStudent.append(x[0])
+
+print(desireStudent)
+
