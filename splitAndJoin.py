a8151e4d833a7b7b30f01d6c503715cd77234af3
duyvu <vuthanhduy93@gmail.com>
2021-08-01 14:54:12 +0700

    finnish the task

--- splitAndJoin.py
+++ splitAndJoin.py
@@ -1,3 +1,13 @@
-txt = 'this is a string'
-txt1 = txt.split('')
+a = 'this is a string'
+txt1 = a.split(' ')
 print (txt1) 
+txt2 = '-'.join(txt1)
+
+
+
+def split_and_join(line):
+    return ('-'.join(line.split(' ')))
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
