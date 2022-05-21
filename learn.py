7dff5b43fbd15ed2d588e9120332c353ae39d691
unknown <vuthanhduy93@gmail.com>
2022-05-21 17:38:23 +0700

    string learn

--- learn.py
+++ learn.py
@@ -1,7 +1,4 @@
-from tkinter import Y
 
-
-print('hello')
 #this is a comment
 #multi line comment
 # secondline
@@ -15,25 +12,63 @@ gfasfsd
 # 2var (numbers at begining of the name)
 # My-var (special signals)
 # my var (spaces)#
-x = int(2)
-x=float(3)
-y='hello'
-print(type(x))
+# x = int(2)
+# x=float(3)
+# y='hello'
+# print(type(x))
 # print(x+y) error command
-print(x,y)
+# print(x,y)
 
-x=y=z=5 #assign variables to a value
-x,y,z=5,6,7
-x,y,z = [6,7,8]
-fruits=['apple', 'banana', 'orange']
-x,y,z=fruits
-print(x,y,z)
+# x=y=z=5 #assign variables to a value
+# x,y,z=5,6,7
+# x,y,z = [6,7,8]
+# fruits=['apple', 'banana', 'orange']
+# x,y,z=fruits
+# print(x,y,z)
 
 
-def myfunc():
-    global x 
-    x='awesome'
-    print('python is '+x)
+# def myfunc():
+#     global x 
+#     x='awesome'
+#     print('python is '+x)
 
-myfunc()
-print(x)
+# myfunc()
+# print(x)
+
+#
+# number : int float complex
+# string: str
+# list, tuple, set, dict, range
+# bool
+# bytes, bytearray,memoryview
+# none#
+# x=5
+# print(type(x))
+#import random library
+# import random
+# print(random.randrange(10))
+# x='''bc'''
+# print(type(x))
+# print(x[1])
+# print(len(x))
+# if 'a' not in x:
+#     print('yes')
+# else:
+#     print('no')
+# for i in x:
+#     print(i)
+b = " Hello, World!  "
+# print(b[2:5])
+# print(b[5:])
+# print(b[-7:-1])
+print(b.upper())
+print(b.lower())
+print(b.strip())
+x=b.split(',')
+print(x) #return x is a list
+a= 'python'
+print(a+b)
+x,y,z=3,4,5
+txt='i want to buy {} eggs with price {} total {}'
+print(txt.format(x,y,z))
+
