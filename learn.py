9c98b3f2b703e1fffdf24ee3afd4337a49254a7f
duyvu <vuthanhduy93@gmail.com>
2022-05-22 12:49:53 +0700

    list review

--- learn.py
+++ learn.py
@@ -57,18 +57,32 @@ gfasfsd
 #     print('no')
 # for i in x:
 #     print(i)
-b = " Hello, World!  "
+# b = " Hello, World!  "
 # print(b[2:5])
 # print(b[5:])
 # print(b[-7:-1])
-print(b.upper())
-print(b.lower())
-print(b.strip())
-x=b.split(',')
-print(x) #return x is a list
-a= 'python'
-print(a+b)
-x,y,z=3,4,5
-txt='i want to buy\' {} \'eggs\n with price {} \n\t\btotal {}'
-print(txt.format(x,y,z))
+# print(b.upper())
+# print(b.lower())
+# print(b.strip())
+# x=b.split(',')
+# print(x) #return x is a list
+# a= 'python'
+# print(a+b)
+# x,y,z=3,4,5
+# txt='i want to buy\' {} \'eggs\n with price {} \n\t\btotal {}'
+# print(txt.format(x,y,z))
 
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist)
+thislist[1:3]=["kiwi"]
+print(thislist)
+x= ['a','b']
+# thislist.append(x)
+# print(thislist)
+# thislist.insert(3,x)
+# print(thislist)
+thislist.extend(x)
+print(thislist)
+
+# del thislist
+print(thislist)
