21ff5c70e634c0d90b0c397db5cf59b0ce9a23b4
duyvu <vuthanhduy93@gmail.com>
2022-05-23 10:47:01 +0700

    review

--- learn2.py
+++ learn2.py
@@ -1,24 +1,40 @@
-# x=y=z='5'
-# print(x,y,z)
+# # x=y=z='5'
+# # print(x,y,z)
 
-# from turtle import clear
+# # from turtle import clear
 
 
-x='apple'
-# for i in x:
-#     print(i)
+# x='apple'
+# # for i in x:
+# #     print(i)
 
-print(x.upper())
-print(x.lower())
-print(x.strip())
+# print(x.upper())
+# print(x.lower())
+# print(x.strip())
 
-y='hello, world'
-z=y.split(',')
-print(z)
-z=y.replace('l','L')
-print(z)
-# print(x+' '+y)
-z=x+' '+y
-txt='i want to buy {2} eggs with {0} milk and {1} coconuts'
-x,y,z=[3,4,5]
-print(txt.format(x,y,z))
+# y='hello, world'
+# z=y.split(',')
+# print(z)
+# z=y.replace('l','L')
+# print(z)
+# # print(x+' '+y)
+# z=x+' '+y
+# txt='i want to buy {2} eggs with {0} milk and {1} coconuts'
+# x,y,z=[3,4,5]
+# print(txt.format(x,y,z))
+
+# print ('hello world with')
+
+mylist=['apple', 'orange','banana', 'apple', 'orange','banana']
+# mylist=mylist*2
+mylist[-3:-1]=['orange', 'apple']
+
+mylist.append('hello')
+mylist.insert(2,"pineapple")
+mylist.extend([2,3,4])
+
+mylist.remove('banana')
+mylist.pop(0)
+# del mylist
+mylist.clear()
+print(mylist)
