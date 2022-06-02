d6f4a0b48f7706d2b47b8d4321958b5c7ec40c87
unknown <vuthanhduy93@gmail.com>
2022-06-02 15:36:47 +0700

    functionrv

--- learn.py
+++ learn.py
@@ -1,30 +1,20 @@
+from calendar import c
+
+
 def myfunc():
-  print('Hello')
-
+  print('hello')
 myfunc()
+def myfunc1(a):
+  print('hello',a)
 
-def myfunc1(name, age):
-  print('hello', name ,' im ', age)
+myfunc1(5)
+def myfunc2(*a):
+  print('hello',a)
+myfunc2(3,4,5)
 
-myfunc1('duy', 8)
-
-def myfunc2(*key):
-  print('i want to buy', key)
-myfunc2('apple', "orange", 'banana')
-
-def myfunc3(child1, child2, child3):
-  print(child1)
-myfunc3(child1='a', child3='b', child2='c')
-
-def myfunc4(**child):
-  print('his name is',child['child1'])
-
-myfunc4(child='a', child1='b')
-
-class TV:
-  def __init__(self, size, year):
-    self.size=size
-    self.year=year    
-
-LG=TV(32,2022)
-print(LG.size, LG.year)
+def myfunc3(child1,child2,child3):
+  print('welcome',child1,child2,child3)
+myfunc3(child1='a',child2='b',child3='c')
+def myfunc4(**child1):
+  print('welcome',child1)
+myfunc4(child1='a', child2='b',child3='c')
