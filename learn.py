604096dd0262ada6e50afaca39fe83ded3a848a2
duyvu <vuthanhduy93@gmail.com>
2022-06-03 10:34:11 +0700

    review

--- learn.py
+++ learn.py
@@ -1,9 +1,10 @@
-def func():
-  print('this is a function')
-func()
-def func2(x):
-  print(x*x)
-func2(5)
-def func3(*x):
-  print(x * 2)
-func3(3,4,5)
+# def func():
+#   print('this is a function')
+# func()
+# def func2(x):
+#   print(x*x)
+# func2(5)
+# def func3(*x):
+#   print(x * 2)
+# func3(3,4,5)
+# def fun
