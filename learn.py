07ca10fe51b33f70d8f5d88a953fdcb744bba6db
unknown <vuthanhduy93@gmail.com>
2022-06-08 10:05:03 +0700

    revire pure python

--- learn.py
+++ learn.py
@@ -1,10 +1,37 @@
-# def func():
-#   print('this is a function')
-# func()
-# def func2(x):
-#   print(x*x)
-# func2(5)
-# def func3(*x):
-#   print(x * 2)
-# func3(3,4,5)
-# def fun
+# a='a string too long'
+# b='''
+# this is
+# a
+# multiline
+# string '''
+# # for x in a:
+# #     print(x)
+# print(a[0])
+# print(a.split('t'))
+# x=5
+# y='b \n \t \' \\{}'
+# print(a+b)
+# print(y.format(x))
+# from re import X
+
+
+list1=['a','b','b','c']
+print(list1[-2:-1])
+list1[0]='x'
+print(list1[0])
+list2=['x','y','z']
+list1.insert(2,'a')
+print(list1)
+list1.extend(list2)
+print(list1)
+print({x for x in list1 if 'x' in x})
+
+
+tuple1=('a','b','c')
+print(type(tuple1))
+tuple1=list(tuple1)
+print(tuple1[1])
+if 'a' in tuple1:
+    print('yes')
+else:
+    print('no')
